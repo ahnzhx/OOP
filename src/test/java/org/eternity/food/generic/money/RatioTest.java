fc5e185f3dde8571e61335e6a22e44c52cd78baf
ahnzhx <modunaeggu@naver.com>
2019-07-16 08:10:28 +0900

    finished first one

---
+++ src/test/java/org/eternity/food/generic/money/RatioTest.java
@@ -0,0 +1,20 @@
+package org.eternity.food.generic.money;
+
+import org.eternity.food.domain.generic.money.Money;
+import org.eternity.food.domain.generic.money.Ratio;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class RatioTest {
+
+    @Test
+    public void 퍼센트(){
+        Ratio tenPercent = Ratio.valueOf(0.1);
+        Money thousandWon = Money.wons(1000);
+
+        assertThat(tenPercent.of(thousandWon), is(Money.wons(100)));
+
+    }
+}
