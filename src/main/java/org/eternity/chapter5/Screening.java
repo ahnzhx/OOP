6331c9b863e1fbb8b71d860fabde121beadbaa9d
ahnzhx <modunaeggu@naver.com>
2020-08-12 07:14:53 +0900

    first step of implementation of Screening

---
+++ src/main/java/org/eternity/chapter5/Screening.java
@@ -0,0 +1,19 @@
+package org.eternity.chapter5;
+
+import org.eternity.bookingTicket.Reservation;
+
+import java.time.LocalDateTime;
+
+public class Screening {
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
+
+    public Reservation reserve(Customer customer, int audienceCount){
+        return new Reservation(customer, this, calculateFee(audienceCount), audienceCount);
+    }
+
+    private Money calculateFee(int audienceCount){
+        return movie.calculateMovieFee(this).times(audienceCount);
+    }
+}
