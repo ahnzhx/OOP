2fbe7def13178148787166fe2865156e70fe243d
ahnzhx <modunaeggu@naver.com>
2020-08-12 18:56:50 +0900

    issue new problem after split DiscountCondition into SequenceCondition and PeriodCondition

--- src/main/java/org/eternity/chapter5/Movie.java
+++ src/main/java/org/eternity/chapter5/Movie.java
@@ -9,7 +9,8 @@ public class Movie {
     private String title;
     private Duration runningTime;
     private Money fee;
-    private List<DiscountCondition> discountConditions;
+    private List<PeriodCondition> periodConditions;
+    private List<SequenceCondition> sequenceConditions;
 
     private MovieType movieType;
     private Money discountAmount;
@@ -47,6 +48,14 @@ public class Movie {
     }
 
     private boolean isDiscountable(Screening screening){
-        return discountConditions.stream().anyMatch(condition -> condition.isSatisfiedBy(screening));
+        return checkPeriodConditions(screening) || checkSequenceConditions(screening);
+    }
+
+    private boolean checkPeriodConditions(Screening screening) {
+        return periodConditions.stream().anyMatch(condition -> condition.isSatisfiedBy(screening));
+    }
+
+    private boolean checkSequenceConditions(Screening screening) {
+        return sequenceConditions.stream().anyMatch(condition -> condition.isSatisfiedBy(screening));
     }
 }
